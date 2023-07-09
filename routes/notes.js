616a2d13d20f321f243e1b6ce88fead22435964d
joshualam071522 <joshualam071522@gmail.com>
2023-07-09 06:04:58 -0700

    added helper function to help read and write files, added get and post routes for notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,38 @@
 const notes = require('express').Router();
 
+//* built-in read/write system that allows us to view files 
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+//* GET route to retrieve stored notes
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  }
+);
+
+//* POST route to add a new note
+notes.post('/', (req, res) => {
+  const { title, text } = req.body;
+
+  if (title && text) {
+    
+    const newNote = {
+      title,
+      text,
+    };
+  
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    
+    console.log(response);
+    res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in creating note');
+    }
+  });
 
 //* exports the notes routes
 module.exports = notes;
